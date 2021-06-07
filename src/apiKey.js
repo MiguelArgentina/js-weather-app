46636594e9b55ab0fa679f25279073de4493c126
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-06-07 19:29:32 -0300

    Refactor code to stop using google maps

--- src/apiKey.js
+++
@@ -1,2 +0,0 @@
-const getKey = () => '';
-export default getKey;
--- src/index.js
+++ src/index.js
@@ -1,44 +1,10 @@
 import 'bootstrap';
-import "./style.scss";
-import getKey from './apiKey';
-import countriesJsonList from './json-lists/ISO3166.country.list.json';
-import citiesJsonList from './json-lists/city.list.min.json';
+import './style.scss';
+import getOpenweatherApiKey from './openweatherApiKey';
+import getMapboxApiKeyKey from './mapboxApiKeyKey';
 
-const apiKey = getKey;
-let citiesNamesList = [];
-let countriesNamesList = [];
+const openweatherApiKey = getOpenweatherApiKey;
 
-Object.keys(countriesJsonList[100]).forEach((key) => {
-  console.log(key, countriesJsonList[100][key]);
-});
-
-Object.keys(citiesJsonList[100]).forEach((key) => {
-  console.log(key, citiesJsonList[100][key]);
-});
-
-async function citiesData() {
-  const citiesNamesArr = [];
-  citiesJsonList.forEach((element) => {
-    citiesNamesArr.push({
-      cityName: element.name,
-      isoCountryName: element.country,
-      id: element.id,
-    });
-  });
-  return citiesNamesArr;
-}
-
-async function countriesData() {
-  const countriesNamesArr = [];
-  countriesJsonList.forEach((element) => {
-    countriesNamesArr.push({ countryName: element.name, countryCode: element['country-code'], isoCountryName: element['alpha-2'] });
-  });
-  return countriesNamesArr;
-}
-
-const citySelected = (e) => {
-  console.log(e.target.childNodes[1].innerText)
-}
 
 const createRow = (lists) => {
   const list = document.getElementById('list');
@@ -55,79 +21,97 @@ const createRow = (lists) => {
   });
 };
 
-function accentsTidy(s) {
-  let r = s.toLowerCase();
-  const nonAsciis = {
-    a: '[àáâãäå]',
-    ae: 'æ',
-    c: 'ç',
-    e: '[èéêë]',
-    i: '[ìíîï]',
-    n: 'ñ',
-    o: '[òóôõö]',
-    oe: 'œ',
-    u: '[ùúûűü]',
-    y: '[ýÿ]',
-  };
-  for (let i in nonAsciis) {
-    r = r.replace(new RegExp(nonAsciis[i], 'g'), i);
+function fetchHandler(response) {
+  if (response.ok) {
+    return response.json().then((json) => Promise.resolve({
+      json,
+      response,
+    }))
+      .catch(() => Promise.resolve({
+        response,
+      }));
   }
-  return r;
+  return response.json().catch(() => {
+    throw new Error(response.statusText);
+  });
 }
 
-async function addCountryInfo(tempArray) {
-  countriesData().then((countriesNamesListFromStorage) => {
-    tempArray.forEach((item) => {
-      countriesNamesListFromStorage.forEach((countryItem) => {
-        if (item.isoCountryName === countryItem.isoCountryName) {
-          Object.assign(item, { countryCode: countryItem.countryCode });
-          Object.assign(item, { countryName: countryItem.countryName });
-        }
-      });
-    });
-  });
-  return tempArray;
+const getWeatherForecast = (citySearchBox) => {
+  let url = 'https://api.openweathermap.org/data/2.5/weather?q=';
+  url += citySearchBox;
+  url += '&appid=';
+  url += getOpenweatherApiKey;
+  return fetch(url);
 };
 
-const searchCity = document.getElementById("searchCity");
+const getFiveDaysWeatherForecast = (citySearchBox) => {
+  let url = 'https://api.openweathermap.org/data/2.5/forecast?q=';
+  url += citySearchBox;
+  url += '&appid=';
+  url += getOpenweatherApiKey;
+  return fetch(url);
+};
+
+function getOneDayForecaste(forecast) {
+  const tempC = Math.round(parseFloat(forecast.main.temp)-273.15);
+  const tempF = Math.round(((parseFloat(forecast.main.temp)-273.15)*1.8)+32); 
+  const feelsC = Math.round(parseFloat(forecast.main.feels_like)-273.15);
+  const feelsF = Math.round(((parseFloat(forecast.main.feels_like)-273.15)*1.8)+32); 
+
+  document.getElementById('description').innerText = forecast.weather[0].description;
+  document.getElementById('temp').innerText = `${tempC}&deg;`;
+  document.getElementById('location').innerText = forecast.name;
+}
+
+const searchCity = document.getElementById('searchCity');
+let currentForecast = '';
+let fiveDaysForecast = '';
+
+const createMap = (lat = -31.4135, lon = -64.1811) => {
+  document.getElementById('weathermap').innerHTML = "<div id='mapid'></div>";
+  const mymap = L.map('mapid').setView([lat, lon], 11);
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: getMapboxApiKeyKey,
+  }).addTo(mymap);
+};
+
 searchCity.addEventListener('click', (e) => {
   e.preventDefault();
-  let tempArray = [];
-  const citySearchBox = document.getElementById("citySearchBox").value;
+  const citySearchBox = document.getElementById('citySearchBox').value;
   if (citySearchBox.length > 2) {
-    const searchBoxText = accentsTidy(citySearchBox).toLowerCase();
-    tempArray = citiesNamesList.filter((city) => accentsTidy(city.cityName).toLowerCase().includes(searchBoxText)
-    );
-    addCountryInfo(tempArray)
-      .then((result) => (tempArray = result))
-      .then((result) =>{
-        createRow(result);
+    getWeatherForecast(citySearchBox)
+      .then((response) => fetchHandler(response))
+      .then((res) => {
+        if (res.cod) {
+          console.log('There was a problem: ', res.message)
+        } else {
+          currentForecast = res.json;
+          console.log(currentForecast);
+          getOneDayForecaste(currentForecast);
+          createMap(currentForecast.coord.lat, currentForecast.coord.lon)
+          // createRow(result);
+        }
       });
-  } else {
-    createRow(["Please enter more than 3 characters"]);
-  }
-});
-
-
-
-const keyword = document.getElementById('keyword');
-keyword.addEventListener('keyup', (e) => {
-  let tempArray = [];
-  console.log(e.target.value);
-  if (e.target.value.length > 2) {
-    const searchBoxText = accentsTidy(e.target.value).toLowerCase();
-    tempArray = citiesNamesList.filter((city) => accentsTidy(city).toLowerCase().includes(searchBoxText)
-    );
-    console.log(tempArray);
-
-    createRow(tempArray);
+    getFiveDaysWeatherForecast(citySearchBox)
+      .then((response) => fetchHandler(response))
+      .then((res) => {
+        if (res.cod) {
+          console.log('There was a problem: ', res.message)
+        } else {
+          fiveDaysForecast = res.json;
+          console.log(fiveDaysForecast);
+          // createRow(result);
+        }
+      });
   } else {
     createRow(['Please enter more than 3 characters']);
   }
 });
 
 window.onload = () => {
-  citiesData().then((citiesNamesListFromStorage) => {
-    citiesNamesList = citiesNamesListFromStorage;
-  });
 };
